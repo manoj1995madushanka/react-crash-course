75ce11fa33c72c59a0ffa8041f8ae5a28b6babf7
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-02-25 16:41:31 +0530

    use effect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const App = () => {
 
     const [counter, setCounter] = useState(0);
 
+    // never modify state manually
+    // use effect state runs when reload the app
+    // React is about components, states and props
+    useEffect(() => {
+        alert("You changed the counter : " + counter);
+    }, [counter])
+
     return (
         <div className="App">
-            <button onClick={()=>setCounter((prevCount)=>prevCount-1)}>-</button>
+            <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
             <h1>{counter}</h1>
-            <button onClick={()=>setCounter((prevCount)=>prevCount+1)}>+</button>
+            <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
         </div>
     )
 }
