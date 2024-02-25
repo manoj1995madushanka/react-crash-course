3149a77662f9924fe86425a6983c3b2e4159c0f8
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-02-25 16:19:13 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Person = (props) => {
+    return (
+        <>
+            <h1>Name: {props.name}</h1>
+            <h2>Last Name: Doe</h2>
+            <h2>Age: 30</h2>
+        </>
+    )
+}
+
+const App = () => {
+    const name = 'John';
+    const isNameShowing = false;
+
+    return (
+        <div className="App">
+            <Person name={'Manoj'}/>
+            <Person/>
+            <Person/>
+            <Person/>
+        </div>
+    );
 }
 
 export default App;
