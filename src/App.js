e86128efa6495bb4f6cbbc5c01237c82f169c88f
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-02-25 16:34:53 +0530

    use states

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import './App.css';
-
-const Person = (props) => {
-    return (
-        <>
-            <h1>Name: {props.name}</h1>
-            <h2>Last Name: Doe</h2>
-            <h2>Age: 30</h2>
-        </>
-    )
-}
+import {useState} from "react";
 
 const App = () => {
-    const name = 'John';
-    const isNameShowing = false;
+
+    const [counter, setCounter] = useState(0);
 
     return (
         <div className="App">
-            <Person name={'Manoj'}/>
-            <Person/>
-            <Person/>
-            <Person/>
+            <button onClick={()=>setCounter((prevCount)=>prevCount-1)}>-</button>
+            <h1>{counter}</h1>
+            <button onClick={()=>setCounter((prevCount)=>prevCount+1)}>+</button>
         </div>
-    );
+    )
 }
 
 export default App;
